cc198df68eb059770d9b6cbb6bc73bb4650f2620
Emilio <>
2021-12-14 18:45:10 -0300

    version final

---
+++ src/zoolife/Gorro.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoolife;
+
+/**
+ *
+ * @author Benjamín Igor 
+ * @author Emilio Contreras 
+ * @author Pablo Martínez
+ */
+//Clase gorro
+public class Gorro extends Producto {
+    
+    public Gorro(String nombre, int valor) {
+        super(nombre, valor);
+    }
+//Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
