7d10861f5cb50e8b1bc054da66a61abb2e7e1007
Emilio361 <96144496+Emilio361@users.noreply.github.com>
2021-12-14 18:55:04 -0300

    Add files via upload

---
+++ src/zoolife/Zoolife.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoolife;
+
+import zoolife.ventanas.JDatosCliente;
+
+/**
+ *
+ * @author Benjamín Igor
+ * @author Emilio Contreras
+ * @author Pablo Martínez
+ */
+//Clase Zoolife
+public class Zoolife {
+//Main
+    public static void main(String[] args) {
+        JDatosCliente cliente = new JDatosCliente();
+        cliente.setVisible(true);
+    }
+}
